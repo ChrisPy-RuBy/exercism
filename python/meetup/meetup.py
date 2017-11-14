7d3ec811871b87d02ac679ea9e78cf00e8fd5a99
Chris Woodall <christopherhwoodall@gmail.com>
2017-11-14 18:27:50 +0000

    first test passed

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -2,25 +2,35 @@ from datetime import date
 
 def meetup_day(year, month, day_of_the_week, which):
     teenth_array = [13,14,15,16,17,18,19]
-    days_of_the_week = {0: "Sunday",
-                             1: "Monday",
-                             2: "Tuesday",
-                             3: "Wednesday",
-                             4: "Thursday",
-                             5: "Friday",
-                             6: "Saturday",
-                            7: "Sunday"}
+    days_of_the_week = { 0: "Monday",
+                         1: "Tuesday",
+                         2: "Wednesday",
+                         3: "Thursday",
+                         4: "Friday",
+                         5:"Saturday",
+                         6: "Sunday",
+                        }
+                         
 
-    target_day = day_of_the_week
     for x in teenth_array:
-      date = date(year, month, x)
-      if date.weekday()
-
-    return date(2013, 5, 13)
-
-
-
-meetup_day(2013, 5, 'Monday', 'teenth'), date(2013, 5, 13))
+        day = date(year, month, x)
+        if days_of_the_week[day.weekday()] == day_of_the_week:
+            return (date(year, month, x))
+        # else:
+        #     continue
 
 
 
+meetup_day(2013, 5, 'Monday', 'teenth')
+
+
+# days_of_the_week = { 0: "Sunday",
+#                      1: "Monday",
+#                      2: "Tuesday",
+#                      3: "Wednesday",
+#                      4: "Thursday",
+#                      5: "Friday",
+#                      6:"Saturday",
+#                      7: "Sunday",
+#                     }
+                        
