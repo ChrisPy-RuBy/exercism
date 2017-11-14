3c3d5784a2182bd6a003d99c2926ce225208dd61
Chris Woodall <christopherhwoodall@gmail.com>
2017-11-14 18:40:24 +0000

    feeling sleepy

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,7 +1,15 @@
 from datetime import date
 
 def meetup_day(year, month, day_of_the_week, which):
-    teenth_array = [13,14,15,16,17,18,19]
+    month_dict = {  '1st': [1,2,3,4,5,6,7],
+                    '2nd': [8,9,10,11,12,13,14],
+                    'teenth': [13,14,15,16,17,18,19],
+                    '3rd': [20,21,22,23,24,25,26],
+                    '4th': [27,28,29,30]
+                    }
+
+
+
     days_of_the_week = { 0: "Monday",
                          1: "Tuesday",
                          2: "Wednesday",
@@ -12,7 +20,7 @@ def meetup_day(year, month, day_of_the_week, which):
                         }
                          
 
-    for x in teenth_array:
+    for x in month_dict[which]:
         day = date(year, month, x)
         if days_of_the_week[day.weekday()] == day_of_the_week:
             return (date(year, month, x))
