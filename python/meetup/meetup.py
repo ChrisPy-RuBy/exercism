7b9a26bdeece6a791cdd8864fb673efc9e081785
Chris Woodall <christopherhwoodall@gmail.com>
2017-12-09 22:26:52 +0000

    not got last yet

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,51 +1,81 @@
 from datetime import date
-import pprint
 
 def generate_month(year, month):
 
-    month_dict = {  '1st': {},
-                    '2nd': {},
-                    'teenth': {},
-                    '3rd': {},
-                    '4th': {}
-                }
-    week_dict = dict()
-    day_counter = 1
+  month_dict = dict()
+  day = 1
 
-    for x in month_dict:
-        while day_counter % 7 != 0 :
-            try:
-                week_dict[day_counter] = date(year, month,day_counter).weekday()
-                day_counter += 1
-            except ValueError:
-                month_dict[x] = week_dict 
-                break
-        else:
-            week_dict[day_counter] = date(year, month,day_counter).weekday()
-            month_dict[x] = week_dict
-            continue
-        day_counter += 1
-    return month_dict
+  while True:
+    try:
+      week_day = date(year, month, day).weekday()
+      month_dict[day] = week_day
+      day += 1
+    except ValueError:
+      break
+  return month_dict
 
-print (generate_month(2016, 2))
+def split_month(month_dict, which):
+
+  split_month = {
+    '1st': dict(),
+    '2nd': dict(),
+    'teenth': dict(),
+    '3rd': dict(),
+    '4th': dict(),
+    '5th': dict()
+  }
+
+  if which == "teenth":
+    for date, day in month_dict.items():
+      if date < 20 and date > 12:
+        split_month['teenth'][date] = day  
+      elif date <= 12 and date > 5:
+        split_month['2nd'][date] = day
+      elif date  <= 5:
+        split_month['1st'][date] = day
+      elif date >= 20 and date <= 26:
+        split_month['3rd'][date] = day
+      elif date > 26:
+        split_month['4th'][date]  = day
+
+  else: 
+    for date, day in month_dict.items():
+      if date < 8:
+        split_month['1st'][date] = day  
+      elif date < 15:
+        split_month['2nd'][date] = day
+      elif date < 22:
+        split_month['3rd'][date] = day
+      elif date < 29:
+        split_month['4th'][date] = day
+      else:
+        split_month['5th'][date]  = day
+    
+
+  # print (split_month)
+  return split_month[which]
+
+
+
 
 def meetup_day(year, month, day_of_the_week, which):
 
-    month_dict = generate_month(year, month)
+  days_of_the_week = { "Monday": 0,
+                       "Tuesday": 1,
+                       "Wednesday": 2,
+                       "Thursday": 3,
+                       "Friday": 4,
+                       "Saturday": 5,
+                       "Sunday": 6,
+                      }
 
-    days_of_the_week = { "Monday": 0,
-                         "Tuesday": 1,
-                         "Wednesday": 2,
-                         "Thursday": 3,
-                         "Friday": 4,
-                         "Saturday": 5,
-                         "Sunday": 6,
-                        }
-                       
+  month_dict = generate_month(year, month)
+  meetup_dict = split_month(month_dict, which)
 
-    print (month_dict[which])
+  for k, v in meetup_dict.items():
+    if days_of_the_week[day_of_the_week] == v:
+      meetup_date = k 
 
+  return date(year, month, meetup_date)
 
-
-
-                        
+# print (meetup_day(2013, 8, 'Wednesday', '2nd'))
---
+++ python/meetup/meetup_v1.py
@@ -0,0 +1,52 @@
+from datetime import date
+import pprint
+
+def generate_month(year, month):
+
+    month_dict = {  '1st': {},
+                    '2nd': {},
+                    'teenth': {},
+                    '3rd': {},
+                    '4th': {}
+                }
+    week_dict = dict()
+    day_counter = 1
+
+    for x in month_dict:
+        while day_counter % 7 != 0 :
+            try:
+                week_dict[day_counter] = date(year, month,day_counter).weekday()
+                day_counter += 1
+            except ValueError:
+                month_dict[x] = week_dict 
+                break
+        else:
+            week_dict[day_counter] = date(year, month,day_counter).weekday()
+            month_dict[x] = week_dict
+            continue
+        day_counter += 1
+    return month_dict
+
+print (generate_month(2016, 2))
+
+def meetup_day(year, month, day_of_the_week, which):
+
+    month_dict = generate_month(year, month)
+
+    days_of_the_week = { "Monday": 0,
+                         "Tuesday": 1,
+                         "Wednesday": 2,
+                         "Thursday": 3,
+                         "Friday": 4,
+                         "Saturday": 5,
+                         "Sunday": 6,
+                        }
+                       
+
+    return (month_dict[which])
+
+
+print (meetup_day(2013, 5, 'Monday', 'teenth'))
+print (meetup_day(2013, 8, 'Monday', 'teenth'))
+
+                        
