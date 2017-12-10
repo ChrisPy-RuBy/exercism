30132c13133dfe8c15fe6cf231525bc406730f84
Chris Woodall <christopherhwoodall@gmail.com>
2017-12-10 00:49:20 +0000

    good shit

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,35 @@
+import string
+
 def rotate(text, key):
-    return text
+
+  alpha = list(string.ascii_lowercase)
+  beta = list(alpha)
+
+    
+  counter = 0 
+  while counter < key:
+    generate_cypher(alpha)
+    counter += 1
+
+  translation_dict = dict()
+  for index, letter in enumerate(beta):
+    translation_dict[letter] = alpha[index]
+
+  return encode(text, translation_dict)
+
+
+def generate_cypher(alpha):
+  alpha.append(alpha.pop(0))
+
+
+def encode(text, cypher):
+  encoded_string = ""
+  for x in text:
+      if x.isupper():
+        x = x.lower()
+        encoded_string += (cypher.get(x, x).upper())
+      else:
+        encoded_string += cypher.get(x, x)
+  return encoded_string
+
+
