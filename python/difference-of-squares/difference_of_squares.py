574404a811f51f2cbf5e9d95f00b53ae3d63f314
Chris Woodall <christopherhwoodall@gmail.com>
2017-12-10 01:11:50 +0000

    difference_of_squares.py

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,21 @@
-def square_of_sum():
-    pass
+def square_of_sum(num): 
+
+  sum = 0 
+  while num > 0:
+    sum += num
+    num -= 1
+  return sum ** 2 
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(num):
+  sum = 0 
+  while num > 0:
+    sum += (num ** 2)
+    num -= 1
+  return sum 
 
 
-def difference():
-    pass
+def difference(num):
+
+  return (square_of_sum(num) - sum_of_squares(num))
+    
