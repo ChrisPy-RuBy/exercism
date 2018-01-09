7388c1301f404a95465589344b0bafe834b19fbc
Chris Woodall <christopherhwoodall@gmail.com>
2018-01-09 20:43:17 +0000

    shit fuck bum cunt

---
+++ python/largest-series-product/largest_series_product.py
@@ -0,0 +1,12 @@
+def largest_product(series, size):
+
+    largest_product = 0  
+    for index, value in enumerate(series):
+      x =  series[index : index + size]
+      running_total = 1
+      for y in x:
+        running_total *= int(y)
+      if running_total > largest_product:
+        largest_product = running_total
+  
+    return largest_product
