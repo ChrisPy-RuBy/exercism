37fbe09fa0abc8b68cd364c7cafbd425d7adf80d
Chris Woodall <christopherhwoodall@gmail.com>
2018-01-15 12:01:41 +0100

    all

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,14 +1,35 @@
 def largest_product(series, size):
 
-  
+  # counter for the largest product
+  largest_product_col = 0
 
-    largest_product = 0  
-    for index, value in enumerate(series):
-      x =  series[index : index + size]
-      running_total = 1
-      for y in x:
+  # a bunch of catches for weird values etc
+  if len(series) < size:
+    raise ValueError("size is larger the series length")
+
+  if len(series) == 0 or size == 0:
+    largest_product_col = 1
+
+  if size < 0:
+    raise ValueError("this is negative")
+
+    
+  for index, value in enumerate(series):
+    x = series[index: index + size]
+    
+    if ("0" in x) or len(x) != size :
+      continue
+
+    running_total = 1
+    for y in x:
+      try:
         running_total *= int(y)
-      if running_total > largest_product:
-        largest_product = running_total
-  
-    return largest_product
+      except ValueError:
+        raise
+    if running_total > largest_product_col:
+      largest_product_col = running_total
+
+  return largest_product_col
+
+
+
--- python/largest-series-product/largest_series_product_test.py
+++ python/largest-series-product/largest_series_product_test.py
@@ -44,48 +44,48 @@ class SeriesTest(unittest.TestCase):
     def test_reports_zero_if_all_spans_include_zero(self):
         self.assertEqual(largest_product("99099", 3), 0)
 
-    # def test_rejects_span_longer_than_string_length(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         largest_product("123", 4)
+    def test_rejects_span_longer_than_string_length(self):
+        with self.assertRaisesWithMessage(ValueError):
+            largest_product("123", 4)
 
-    # def test_reports_1_for_empty_string_and_empty_product_0_span(self):
-    #     self.assertEqual(largest_product("", 0), 1)
+    def test_reports_1_for_empty_string_and_empty_product_0_span(self):
+        self.assertEqual(largest_product("", 0), 1)
 
-    # def test_reports_1_for_nonempty_string_and_empty_product_0_span(self):
-    #     self.assertEqual(largest_product("123", 0), 1)
+    def test_reports_1_for_nonempty_string_and_empty_product_0_span(self):
+        self.assertEqual(largest_product("123", 0), 1)
 
-    # def test_rejects_empty_string_and_nonzero_span(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         largest_product("", 1)
+    def test_rejects_empty_string_and_nonzero_span(self):
+        with self.assertRaisesWithMessage(ValueError):
+            largest_product("", 1)  
 
-    # def test_rejects_invalid_character_in_digits(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         largest_product("1234a5", 2)
+    def test_rejects_invalid_character_in_digits(self):
+        with self.assertRaisesWithMessage(ValueError):
+            largest_product("1234a5", 2)
 
-    # def test_rejects_negative_span(self):
-    #     with self.assertRaisesWithMessage(ValueError):
-    #         largest_product("12345", -1)
+    def test_rejects_negative_span(self):
+        with self.assertRaisesWithMessage(ValueError):
+            largest_product("12345", -1)
 
-    # @unittest.skip("extra-credit")
-    # def test_project_euler_big_number(self):
-    #     series = (
-    #         "73167176531330624919225119674426574742355349194934969835203127745"
-    #         "06326239578318016984801869478851843858615607891129494954595017379"
-    #         "58331952853208805511125406987471585238630507156932909632952274430"
-    #         "43557668966489504452445231617318564030987111217223831136222989342"
-    #         "33803081353362766142828064444866452387493035890729629049156044077"
-    #         "23907138105158593079608667017242712188399879790879227492190169972"
-    #         "08880937766572733300105336788122023542180975125454059475224352584"
-    #         "90771167055601360483958644670632441572215539753697817977846174064"
-    #         "95514929086256932197846862248283972241375657056057490261407972968"
-    #         "65241453510047482166370484403199890008895243450658541227588666881"
-    #         "16427171479924442928230863465674813919123162824586178664583591245"
-    #         "66529476545682848912883142607690042242190226710556263211111093705"
-    #         "44217506941658960408071984038509624554443629812309878799272442849"
-    #         "09188845801561660979191338754992005240636899125607176060588611646"
-    #         "71094050775410022569831552000559357297257163626956188267042825248"
-    #         "3600823257530420752963450")
-    #     self.assertEqual(largest_product(series, 13), 23514624000)
+    @unittest.skip("extra-credit")
+    def test_project_euler_big_number(self):
+        series = (
+            "73167176531330624919225119674426574742355349194934969835203127745"
+            "06326239578318016984801869478851843858615607891129494954595017379"
+            "58331952853208805511125406987471585238630507156932909632952274430"
+            "43557668966489504452445231617318564030987111217223831136222989342"
+            "33803081353362766142828064444866452387493035890729629049156044077"
+            "23907138105158593079608667017242712188399879790879227492190169972"
+            "08880937766572733300105336788122023542180975125454059475224352584"
+            "90771167055601360483958644670632441572215539753697817977846174064"
+            "95514929086256932197846862248283972241375657056057490261407972968"
+            "65241453510047482166370484403199890008895243450658541227588666881"
+            "16427171479924442928230863465674813919123162824586178664583591245"
+            "66529476545682848912883142607690042242190226710556263211111093705"
+            "44217506941658960408071984038509624554443629812309878799272442849"
+            "09188845801561660979191338754992005240636899125607176060588611646"
+            "71094050775410022569831552000559357297257163626956188267042825248"
+            "3600823257530420752963450")
+        self.assertEqual(largest_product(series, 13), 23514624000)
 
     # Utility functions
     def setUp(self):
