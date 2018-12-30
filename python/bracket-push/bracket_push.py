f868e64f2818579e5662784fd399c19f61de908c
chris <christopherhwoodall@gmail.com>
2018-12-30 11:27:00 +0000

    bash test

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,21 +1,32 @@
 def is_paired(input_string):
-
-    openbracket = {}
+    
+    lookup = {"{": "}", 
+              "[": "]",
+              "(": ")"}
     result = True
 
-    for char in input_string: 
-        if char in ['[', '(', '{']:
-            openbracket[char] = True
-        if char in [']', ')', '}'] and openbracket.get(char, None):
-            openbracket[char] = False
+    for index, letter in enumerate(input_string):
+        print(letter)
+        if letter in lookup.keys():
+            for index, newletter in enumerate(input_string[index:]):
+                print('1', index, newletter, input_string[index:])
+                if newletter == lookup[letter]:
+                    input_string.pop(index)
 
-    if True in openbracket.values():
-      result = False
+                elif newletter in lookup.values() and newletter != lookupletter:
+                    result = False
+                elif newletter not in lookup.values():
+                    result = False
+
+
+
+    
     return result
 
 
+print(is_paired('{}'))
+print(is_paired('}{'))
 
-
-print(is_paired('[]'))
+print(is_paired('([{])'))
             
 
