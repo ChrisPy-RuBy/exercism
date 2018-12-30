cc159d7d7293f1cab03f9290419f192dabd5e61f
chris <christopherhwoodall@gmail.com>
2018-12-30 17:40:45 +0000

    all tests pass

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,8 +1,25 @@
+#!/usr/bin/env bash 
+if [ $1 == 'total' ] 
+    then
+        total="0"
+        i="64"
+        while [ $i -gt 0 ]
+        do
+        i=$[$i-1]
+        var=$(echo 2^$i | bc)
+        total=$(echo $total+$var | bc)
+        done
+    echo $total
+    exit 0 
+fi
+
+
+
 error="Error: invalid input"
 if [ $1 -gt 0 ] && [ $1 -lt 65 ]
-then
-    let power=$1-1
-    python -c "x = 2**$power; print x"
+    then
+        let power=$1-1
+        python -c "x = 2**$power; print x"
 else
     echo $error
     exit 1
---
+++ bash/grains/testfunction.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+test_function () { 
+    echo hello world click piss
+}
