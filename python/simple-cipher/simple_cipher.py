5efd898e86a09a5c1a27dc0f251caf42fc7ee5b4
chris <christopherhwoodall@gmail.com>
2019-02-05 18:12:20 +0000

    test2

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -3,6 +3,8 @@ class Cipher(object):
             "This is a really looooooooooooooooooooooooooooooooooooooooooooooog comment"
 
     def encode(self, text):
+        x=1
+        x =   6
         pass
 
     def decode(self, text):
