a513ef7367c7210afdd1884ced915e5232c6c9de
chris <christopherhwoodall@gmail.com>
2019-02-05 19:29:59 +0000

    here goes nothing

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,6 +1,6 @@
 class Cipher(object):
     def __init__(self, key=None):
-            "This is a really looooooooooooooooooooooooooooooooooooooooooooooog comment"
+            "T his is a really looooooooooooooooooooooooooooooooooooooooooooooog comment"
 
     def encode(self, text):
         x=1
