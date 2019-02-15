271836767ca78693db37b5e8a193115f3ee0be7d
chris <christopherhwoodall@gmail.com>
2019-02-15 20:42:02 +0000

    test

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,15 +1,15 @@
 class Cipher(object):
     def __init__(self, key=None):
-            "T h  is is a really looooooooooooooooooooooooooooooooooooooooooooooog comment"
+        "T h  i sda really looooooooooooooooooooooooooooooooooooooooooooooog comment"
 
     def encode(self, text):
-        x=1
-        x =   6
+        x = 1
+        x = 6
         pass
 
     def decode(self, text):
         try:
-            print('test')
+            print("test")
         except:
-            print('fuck u pep8 ')
+            print("fuck u pep8 ")
         pass
