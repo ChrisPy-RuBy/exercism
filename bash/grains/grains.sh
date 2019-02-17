396b4688c52946808246dbc412d325949792ed0f
chris <christopherhwoodall@gmail.com>
2019-02-17 17:47:13 +0000

    updated comments

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash 
+
+#useful articles
+# https://stackoverflow.com/questions/13111967/shell-scripting-raise-to-the-power
+# https://unix.stackexchange.com/questions/40786/how-to-do-integer-float-calculations-in-bash-or-other-languages-frameworks
+
+
+
+
 if [ $1 == 'total' ] 
     then
         total="0"
@@ -17,7 +25,8 @@ error="Error: invalid input"
 if [ $1 -gt 0 ] && [ $1 -lt 65 ]
     then
         let power=$1-1
-        python -c "x = 2**$power; print x"
+        x=$(echo 2^$power | bc)
+        echo $x
 else
     echo $error
     exit 1
