23dd86dee17ffec0c717d4e9deb28b51fb3ff1c5
chris <christopherhwoodall@gmail.com>
2019-02-18 08:15:59 +0000

    updated after mentor comments

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ]
-then
-    echo One for you, one for me.
-else
-    echo One for $1, one for me.
-fi
+
+# default input parameter syntax
+# more information on this can be found 
+# https://stackoverflow.com/questions/9332802/how-to-write-a-bash-script-that-takes-optional-input-arguments
+# and
+# https://www.gnu.org/software/bash/manual/bashref.html#Shell-Parameter-Expansion
+var=${1-you}
+
+echo "One for $var, one for me."
