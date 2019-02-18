389d6a09d9b6f35ca6bb50db89eb1c8941cdb201
chris <christopherhwoodall@gmail.com>
2019-02-18 13:11:38 +0000

    this should fail

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,4 +1,9 @@
 
 
 def two_fer(name='you'):
+
+    x=0
+    y=x+4
+    #pppppppppppppppppppppppppppppppppppppppppppppppppppppeeeeeeeeeeeeeeeeeeeeeeeeepppppppppppppppppppp8
+
     return "One for {}, one for me.".format(name)
