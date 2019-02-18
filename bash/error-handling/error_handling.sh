722772c70a71f898bd2cacbebfef444e79b51f43
chris <christopherhwoodall@gmail.com>
2019-02-18 22:17:05 +0000

    test

---
+++ bash/error-handling/error_handling.sh
@@ -0,0 +1,14 @@
+#!/usr.bin/env bash
+#set -u
+#set -x
+
+if [[ $# == 0 ]]
+then
+    echo "Usage: ./error_handling <greetee>"
+    exit 1
+elif [[ $# -ne 1 ]]
+then
+    exit 1
+else
+    echo "Hello, $1"
+fi
