ee44ef01201424d68a7391236fa24d34da8785e0
chris <christopherhwoodall@gmail.com>
2019-02-22 22:32:40 +0000

    raindrops again

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,20 @@
 def raindrops(number):
-    :
+    resultstring = str(number)
+
+    def ing_ang_ong(number, tocheck):
+        lookup = {
+                  3: 'Pling',
+                  5: 'Plang',
+                  7: 'Plong',
+                 }
+        if number % tocheck == 0:
+            return lookup[tocheck]
+        return ""
+
+    x = [ing_ang_ong(number, 3),
+         ing_ang_ong(number, 5),
+         ing_ang_ong(number, 7)]
+
+    if any(x):
+        resultstring = "".join(x)
+    return resultstring
