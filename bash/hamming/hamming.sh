f8440837cfcf87052f93a1eba453b5cf57790b1c
Chris Woodall <christopherhwoodall@gmail.com>
2019-03-03 21:46:59 +0000

    test

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -4,34 +4,20 @@
 
 strand1=$1
 strand2=$2
-len1="${#strand1}"
-len2="${#strand2}"
-i=0
-wrongcharacters=0
+wrongchars=0
 
-if [[ $len1 != "$len2" ]]
-then
+if [[ "${#strand1}" != "${#strand2}" ]]; then
     echo "left and right strands must be of equal length"
     exit 1
-elif [[ $# == 0 ]]
-then
+elif [[ $# == 0 ]]; then
     echo "Usage: hamming.sh <string1> <string2>"
     exit 1
 fi
 
-if [[ $strand1 = "$strand2" ]]
-then
-    echo $wrongcharacters
-else
-    while [ $i -lt "$len1" ]
-    do
-        totest1=${strand1:$i:1}
-        totest2=${strand2:$i:1}
-        if [[ $totest1 != "$totest2" ]]
-        then
-            ((wrongcharacters++))
-        fi
-        ((i++))
-    done
-    echo $wrongcharacters
-fi
+for (( i=0; i<${#strand1};i++ ))
+do
+    if [[ ${strand1:$i:1} != ${strand2:$i:1} ]]; then
+        ((wrongchars++))
+    fi
+done
+echo $wrongchars
