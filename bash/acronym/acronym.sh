c99a07b7ed8e13420dca9d17fe7d976748a3b34e
Chris Woodall <christopherhwoodall@gmail.com>
2019-03-12 19:41:52 +0000

    kermit the fraawg

---
+++ bash/acronym/acronym.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# useful articles
+# https://www.lifewire.com/pass-arguments-to-bash-script-2200571 
+# https://aty.sdsu.edu/bibliog/latex/debian/bash.html
+# http://tldp.org/LDP/abs/html/string-manipulation.html -> docs on string manip
+
+# regex for characters that aren't letters or numbers
+clean=${@//[^a-zA-Z0-9]/ }
+result=""
+for para in $clean; do
+    result=$result${para:0:1}
+done
+echo ${result^^}
+
+
--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,14 +1,16 @@
-#!/usr.bin/env bash
+#!/usr/bin/env bash
 #set -u
 #set -x
 
-if [[ $# == 0 ]]
-then
+main () {
+if [[ $# == 0 ]]; then
     echo "Usage: ./error_handling <greetee>"
     exit 1
-elif [[ $# -ne 1 ]]
-then
+elif [[ $# -ne 1 ]]; then
     exit 1
 else
     echo "Hello, $1"
 fi
+}
+
+main "$@"
