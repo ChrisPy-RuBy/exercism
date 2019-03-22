a1c1319e40e9bde7a2a0ec9d2fe95c13db96fc42
Chris Woodall <christopherhwoodall@gmail.com>
2019-03-22 23:27:06 +0000

    fuck shit bum

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -4,9 +4,12 @@ var=$1
 length=${#var}
 sum=0
 
+# create the total
 for (( i=0; i<length; i++ )); do
-    (( sum += $((${var:$i:1}**length))))
+    # this is a bit hard to understand 
+    ((sum += $((${var:$i:1}**length))))
 done
+# decide if armstrong number or not.
 if [[ $var == "$sum" ]]; then
     echo true
 else
