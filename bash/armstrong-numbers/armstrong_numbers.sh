502059d102857da828776c86baab33c747a0fd0f
Chris Woodall <christopherhwoodall@gmail.com>
2019-04-19 18:48:58 +0100

    blerp

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,17 +1,32 @@
 #!usr/bin/env bash
 
-var=$(( $1))
+var=$1
 length=${#var}
-sum=0
 
 # create the total
-for (( i=0; i<length; i++ )); do
-    # this is a bit hard to understand 
-    ((sum += $((${var:$i:1}**length))))
-done
-# decide if armstrong number or not.
-if [[ $var == "$sum" ]]; then
-    echo true
-else
-    echo false
-fi
+sum () {
+    sum=0
+    for (( i=0; i<length; i++ )); do
+        # this is a bit hard to understand 
+        ((sum += $((${var:$i:1}**length))))
+    done
+}
+
+check () {
+    # decide if armstrong number or not.
+    if (( $var == "$sum" )); then
+        echo true
+    else
+        echo false
+    fi
+}
+
+main() {
+
+    var=$1
+    length=${#var}
+    sum
+    check
+}
+
+main $1
