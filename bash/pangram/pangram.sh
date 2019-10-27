bd8aff520cc3c07ec0b500fb151a7e6561e4de79
Chris Woodall <christopherhwoodall@gmail.com>
2019-10-27 23:12:38 +0000

    derp

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -1,16 +1,11 @@
 #! usr/bin/env bash
 
-declare -a arr=("a" "b" "c" "d" "e" "f" "g" "h"
-                "i" "j" "k" "l" "m" "n" "o" "p"
-                "q" "r" "s" "t" "u" "v" "w" "y"
-                "x" "z")
-
 function main () {
     # convert to lowercase
     var=${1,,}
-    for i in "${arr[@]}"; do
+    for i in {a..z}; do
         if ! [[ $i =~ [$var] ]]; then
-            echo false && exit 0
+            echo false; exit 0
         fi
     done
     echo true
