551f130e872f6877f17752ee77dd75c57dbde3bb
Chris Woodall <christopherhwoodall@gmail.com>
2019-11-11 21:18:52 +0000

    update

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,41 +1,34 @@
 #!usr/bin/env bash
-# set -x
-# rewrite this as a function.
-# use a case statement
-# Bob answers '"Sure.' if you ask him a question.
-# He answers 'Whoa, chill out!' if you yell at him.
-# He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-# He says 'Fine. Be that way!' if you address him without actually saying anything.
-# He answers 'Whatever.' to anything else.
 
-answer1="Sure."
-answer2="Whoa, chill out!"
-answer3="Calm down, I know what I'm doing!"
-answer4="Fine. Be that way!"
-answer5="Whatever."
-
-notabs="${1//[$'\t\r\n ']}"
-var="${notabs//[1-9,\.!%\^*@#$:)\t\\]/}"
-clean="${var// /}"
-
-if [[ ${#notabs} -eq 0 ]]; then
-   echo "$answer4" && exit 0
-elif [[ ${#clean} -eq 0 ]]; then
-   echo "$answer5" && exit 0
-fi
+declare -A answers
+answers[sure]="Sure."
+answers[whoa]="Whoa, chill out!"
+answers[calm]="Calm down, I know what I'm doing!"
+answers[fine]="Fine. Be that way!"
+answers[whatever]="Whatever."
 
 function main () {
+
+    notabs="${1//[[:space:]]}"
+    clean="${notabs//[1-9,\.!%\^*@#$:)\\]/}"
+
+    if [[ ${#notabs} -eq 0 ]]; then
+        echo "${answers[fine]}" && exit 0
+    elif [[ ${#clean} -eq 0 ]]; then
+        echo "${answers[whatever]}" && exit 0
+    fi
+
     if [[ $clean =~ \?$ ]]; then
         if [[ $clean =~ ^[^a-z]*$ ]] && [[ ${#clean} -gt 1 ]]; then
-            echo "$answer3" && exit 0
+            echo "${answers[calm]}" && exit 0
         else
-            echo "$answer1" && exit 0
+            echo "${answers[sure]}" && exit 0
         fi
     elif [[ $clean =~ ^[^a-z]*$ ]]; then
-        echo "$answer2" && exit 0
+        echo "${answers[whoa]}" && exit 0
     else
-        echo "$answer5"
+        echo "${answers[whatever]}"
     fi
 }
 
-main "$clean"
+main "$@"
