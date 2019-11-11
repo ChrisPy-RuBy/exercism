6ef13a5a88e4545c476844c1ffb49e60a2db5a40
Chris Woodall <christopherhwoodall@gmail.com>
2019-11-11 21:48:29 +0000

    test

--- bash/rna-transcription/rna_transcription.sh
+++ bash/rna-transcription/rna_transcription.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
-if [[ "${#1}" -gt 0 ]]; then
-    input=$1
-else
-    exit 0
-fi
-
 declare -A dna
 dna=(["G"]="C" ["C"]="G" ["T"]="A" ["A"]="U")
 
 main () {
+
+    if [[ "${#1}" -gt 0 ]]; then
+        input=$1
+    else
+        exit 0
+    fi
+
+    result=""
     for ((i=0; i<${#input}; i++)); do
         rna_letter=${dna[${input:$i:1}]}
         if [[ $rna_letter == "" ]]; then
             echo "Invalid nucleotide detected."; exit 1
         else
-            echo "$rna_letter"
+            result="$result$rna_letter"
         fi
     done
+    echo "$result"
    }
 
 
