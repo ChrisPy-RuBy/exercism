adb90123b74f4493572d6a7dd97048f3101e6cdc
Chris Woodall <christopherhwoodall@gmail.com>
2019-11-17 17:37:30 +0000

    tidied

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-#set -x
 
-strand1=$1
-strand2=$2
-wrongchars=0
+main () {
+    strand1=$1
+    strand2=$2
+    wrongchars=0
 
-if [[ "${#strand1}" != "${#strand2}" ]]; then
-    echo "left and right strands must be of equal length"
-    exit 1
-elif [[ $# == 0 ]]; then
-    echo "Usage: hamming.sh <string1> <string2>"
-    exit 1
-fi
-
-for (( i=0; i<${#strand1};i++ ))
-do
-    if [[ ${strand1:$i:1} != ${strand2:$i:1} ]]; then
-        ((wrongchars++))
+    if [[ "${#strand1}" != "${#strand2}" ]]; then
+        echo "left and right strands must be of equal length"; exit 1
+    elif [[ $# == 0 ]]; then
+        echo "Usage: hamming.sh <string1> <string2>"; exit 1
     fi
-done
-echo $wrongchars
+
+    for (( i=0; i<${#strand1};i++ )); do
+        if [[ ${strand1:$i:1} != "${strand2:$i:1}" ]]; then
+            ((wrongchars++))
+        fi
+    done
+    echo $wrongchars
+}
+main "$@"
