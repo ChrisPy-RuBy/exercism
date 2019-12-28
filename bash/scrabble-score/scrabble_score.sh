4c79a1dad0a363c0ec81e3d98bf338db4aa20ec1
Chris Woodall <christopherhwoodall@gmail.com>
2019-12-28 12:16:38 +0000

    first pass

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+declare -A scrabble
+scrabble["aeioulnrst"]=1
+scrabble["dg"]=2
+scrabble["bcmp"]=3
+scrabble["fhvwy"]=4
+scrabble["k"]=5
+scrabble["jx"]=8
+scrabble["qz"]=10
+
+
+main () {
+    score=0
+    for ((i=0; i<${#1}; i++)); do
+        letter=${1:i:1}
+        for key in "${!scrabble[@]}"; do
+            if [[ $letter =~ [$key] ]]; then
+                value=${scrabble[$key]}
+                (( score += value ))
+            fi
+        done
+    done
+    echo "$score"
+}
+main "$@"
+
